c6fb29ae28c51ac2d003af0f5fb324a57406cece
simon from VSC <shsia125@DESKTOP-BAH1Q4I.localdomain>
2022-03-06 19:51:01 -0500

    edited index.html and todolist.js files

--- todolist.js
+++ todolist.js
@@ -1,6 +1,10 @@
 //Function created to add a task to the task list
+var _counter = 0;
 function addTask() {
-    
+    _counter++;
+    var oClone = document.getElementById("template").cloneNode(true);
+    oClone.id += (_counter + "");
+    document.getElementById("placeholder").appendChild(oClone);
 }
 //Function created to delete a task from the task list
 function deleteTask() {
